d76f76b5fb14dccefaa0a32129bed3d4e0ef3a27
Ishaq Afridi <ishaqafridi@example.com>
2026-05-21 03:13:39 +0500

    new things are adding

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import gymImage from '../../assets/gymImage.png'
+import './about.css'
+
 
 const About = () => {
   return (
     <div>
-      <h1>About Page</h1>
+      <h1>HELLO EVERY ONE ARE LOOKING FOR PARACTICE</h1>
+      <div className='machine-box'>
+        <h1>MACHINES</h1>
+        <img src={gymImage} alt="" />
+        <p>Here are some of our state-of-the-art machines for your workout needs.</p>
+      </div>
     </div>
   )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,13 +5,23 @@ import { div } from 'framer-motion/client'
 
 const Home = () => {
   return (
-    <div>
+    <div className='home'>
       <div className='gymImage'>
         <img src={gymImage} alt="Gym"  />
       </div>
       <div className= "homeText">
         <h1>SCULPT  <span>YOUR</span>  BODY,<br/> ELVETE  <span>YOUR</span>  SPIRIT</h1>
+        <div className='sideText'>
+          <h4><span>P</span><span>R</span><span>E</span><span>V</span></h4>
+          <h4><span>N</span><span>E</span><span>X</span><span>T</span></h4>
+        </div>
+        <div>
+          <div className='InfoText'>
+            <h2>Hello this is Ishaq Afridi's Gym You build your Body Here and Build Your stamina.</h2>
+          </div>
+        </div>
       </div>
+      
     </div>
   )
 }
